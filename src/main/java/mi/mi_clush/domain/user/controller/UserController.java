b7148e50a6f8c36485f623dd18cdb66bc721b61a
mi <argerdle@naver.com>
2025-02-16 14:01:22 +0900

    fix: Swagger v3 적용

--- src/main/java/mi/mi_clush/domain/user/controller/UserController.java
+++ src/main/java/mi/mi_clush/domain/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package mi.mi_clush.domain.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import mi.mi_clush.domain.user.dto.UserReqDto;
 import mi.mi_clush.domain.user.service.UserService;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "회원 관련 API", description = "회원 관련 API")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원 가입", description = "회원 가입")
     @PostMapping("/register")
     public ApiResponse<?> register(@RequestBody UserReqDto request) {
         return ResponseUtils.success(userService.register(request));
